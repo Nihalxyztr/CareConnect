1457d1cfa82875585bdd2a08c240bbfcacc0a8a4
akshay-tiwari333 <tiwariakshay799@gmail.com>
2025-11-25 11:26:11 +0530

    send emailfile

---
+++ utils/sendEmail.js
@@ -0,0 +1,19 @@
+// utils/sendEmail.js
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+});
+
+module.exports = async function sendEmail(to, subject, message) {
+    await transporter.sendMail({
+        from: `"CareConnect" <${process.env.EMAIL_USER}>`,
+        to,
+        subject,
+        html: message
+    });
+};
